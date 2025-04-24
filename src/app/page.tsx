426bde2aa1f167dcaffef097d7446b103f53b769
Julien Le Mee <julemee@gmail.com>
2025-04-24 00:15:32 -0400

    Refactor Home component layout for improved visual design
    - Introduce a blurred background effect for enhanced aesthetics.
    - Adjust main container structure for better responsiveness and positioning.
    - Maintain existing functionality while improving overall user experience.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,25 +9,30 @@ export default async function Home() {
     redirect("/priorities");
   } else {
     return (
-      <main className="flex min-h-screen flex-col items-center justify-center p-4">
-        <div className="container flex flex-col items-center justify-center gap-8 px-4">
-        <h1 className="text-5xl font-extrabold tracking-tight text-center sm:text-[5rem]">
-          Task<span className="text-primary">Forge</span>
-        </h1>
-        <p className="lg:text-xl sm:text-base text-center max-w-2xl text-muted-foreground">
-          Organisez efficacement vos projets.
-        </p>
-        {/* TODO: Add a login button and a signup button */}
-        <div className="flex gap-4">
-          <Link href="/auth/signin">
-            <Button>Login</Button>
-          </Link>
-          <Link href="/auth/signup">
-            <Button variant="outline">Signup</Button>
-          </Link>
-          </div>
-        </div>
-      </main>
+        <>
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[500px] h-[500px] bg-primary/10 rounded-full blur-3xl" />
+
+            <main className="flex min-h-screen flex-col items-center justify-center p-4 relative overflow-hidden">
+
+            <div className="container flex flex-col items-center justify-center gap-8 px-4 relative z-10">
+            <h1 className="text-5xl font-extrabold tracking-tight text-center sm:text-[5rem]">
+                Task<span className="text-primary">Forge</span>
+            </h1>
+            <p className="lg:text-xl sm:text-base text-center max-w-2xl text-muted-foreground">
+                Organisez efficacement vos projets.
+            </p>
+            {/* TODO: Add a login button and a signup button */}
+            <div className="flex gap-4">
+                <Link href="/auth/signin">
+                <Button>Login</Button>
+                </Link>
+                <Link href="/auth/signup">
+                <Button variant="outline">Signup</Button>
+                </Link>
+            </div>
+            </div>
+        </main>
+      </>
     );
   }
 }
